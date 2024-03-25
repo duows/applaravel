606096e981d331e4813e5d2afac11218805c6439
duows <henriquesouzaoficil004@gmail.com>
2024-03-25 11:32:49 -0300

    html added!

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,7 @@
-<header></header>
+<header class="app-header"><a class="app-header__logo" href="index.html">Vali</a>
+    <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+    <!-- Navbar Right Menu-->
+    <!-- <ul class="app-nav">
+
+    </ul> -->
+</header>
