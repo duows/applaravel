608ef1746142d4b8cce141b3ef38e192992ac875
duows <henriquesouzaoficil004@gmail.com>
2024-03-25 11:43:01 -0300

    new files!

--- resources/views/autor/create.blade.php
+++ resources/views/autor/create.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.app')
 @section('content')
-    <div>
-        <form action="{{ route('autor.store') }}" method="POST">
-        @csrf    
-        @include('autor.__form')
-            <button type="submit">Salvar Registro</button>
-        </form>
+<div>
+    <div class="app-title">
+        <div>
+            <h1><i class="bi bi-speedometer"></i> Cadastro</h1>
+            <p>Cadastro de novos autores!</p>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
+            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+        </ul>
     </div>
+
+    <div class='tile'>
+        <div clase='tile-body'>
+            <form action="{{ route('autor.store') }}" method="POST">
+                @csrf
+                @include('autor.__form')
+                <button type="submit">Salvar Registro</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
